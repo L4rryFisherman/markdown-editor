537e7186e93f2d14066d77e4b27e21479d3090cb
L4rryFisherman <99208478+L4rryFisherman@users.noreply.github.com>
2022-06-15 00:39:50 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { FaWindowMaximize, FaWindowMinimize } from 'react-icons/fa';
+import Markdown from 'marked-react';
+import Lowlight from 'react-lowlight'
+import javascript from 'highlight.js/lib/languages/javascript';
+Lowlight.registerLanguage('js', javascript);
+
+const renderer = {
+  code(snippet, lang) {
+    return <Lowlight key={this.elementId} language={lang} value={snippet} />;
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input />
     </div>
   );
 }
+
+
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      folded:true
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    document.body.style.backgroundColor = "#f0f2f5"
+}
+  handleClick() {this.setState(prevState => ({folded: !prevState.folded}));  }
+  render() {
+    return (
+    <div id="input">
+      <div id="inputbox"></div>
+        <div id="inputheader"><h3>Editor</h3>
+          <div id="icon">
+            <button onClick={this.handleClick} >{this.state.folded ===true? <FaWindowMaximize /> :<FaWindowMinimize />}</button>
+          </div>
+        </div>
+      <InputBox folded={this.state.folded} />
+    </div>
+    )
+  }
+}
+
+const defaultinput = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+\/\/ this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
+
+
+class InputBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: defaultinput,
+      output: <Markdown renderer={renderer} value={defaultinput} /> 
+    };
+    this.handleChange = this.handleChange.bind(this);
+  };
+  handleChange = (event) => {
+    let text = event.target.value;
+    text = <ReactMarkdown remarkPlugins={[remarkGfm]}
+    >{text}</ReactMarkdown>;
+    this.setState({ 
+      input: event.target.value,
+      output: text 
+    });
+  }
+  render() {
+    const style = this.props.folded==true ? {'height':'200px'} : {'height':'800px'}
+    return (
+      <div id="textarea">
+      <textarea id="editor" value={this.state.input} onChange={this.handleChange} style={style} />
+      <div id="outputbox">
+      <OutputBox input={this.state.output}/>
+      </div>
+    </div>
+    )
+  }
+}
+
+
+
+ 
+class OutputBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      folded:true
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+    handleClick() {this.setState(prevState => ({folded: !prevState.folded}))};
+  render() {
+    const style = this.state.folded==true ? {'height':'200px'} : {'height':'800px'}
+    return (
+    <div>
+      <div id="outputheader"><h3>Markdown</h3>
+        <div id="icon">
+          <button onClick={this.handleClick} >{this.state.folded ===true? <FaWindowMaximize /> :<FaWindowMinimize />}</button>
+        </div>
+      </div>
+      <div id="preview">
+      <div id="outputtext" style={style}>
+        {this.props.input}
+      </div>
+      </div>
+    </div>
+    )
+  }
+}
+
+
 
 export default App;
